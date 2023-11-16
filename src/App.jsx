4a8ae5697f7ba6b845de4c3b4bc00940d43d2edb
IbrahimGazi_2022~ <coder.ibrahimgazi@gmail.com>
2023-11-17 05:20:54 +0600

    Complete Scroll in html/css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section>1</section>
+      <section>2</section>
+      <section>3</section>
+      <section>4</section>
+      <section>5</section>
+      <section>6</section>
+      <section>7</section>
+      <section>8</section>
+    </div>
+  );
+};
+
+export default App;
